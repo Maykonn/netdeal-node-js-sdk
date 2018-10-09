31ea75fd4fc57ac6ede63899bd98d72136e12238
Maykonn <maykonn@outlook.com>
2018-10-09 18:55:19 -0300

    The key value is immutable

--- src/entity/Consumer.js
+++ src/entity/Consumer.js
@@ -2,6 +2,16 @@ const Entity = require('./Entity.js');
 
 class Consumer extends Entity {
 
+  constructor() {
+    super();
+
+    this._key = 'consumer';
+  }
+
+  set key(value) {
+    throw new Error('The Consumer entity key is immutable');
+  }
+
 }
 
 module.exports = Consumer;
