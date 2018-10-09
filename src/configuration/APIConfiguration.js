c387046657cbf698e3f3c20e105f1b87747b3ccd
Maykonn <maykonn@outlook.com>
2018-10-09 16:20:34 -0300

    Configuration options

--- src/configuration/APIConfiguration.js
+++ src/configuration/APIConfiguration.js
@@ -31,7 +31,21 @@ module.exports = {
   /**
    * API Resources Configuration
    *
-   * @type {{requestAccessToken: string, sendEntity: string}}
+   * @see http://www.netdeal.com.br/documentation/
    */
-  ...require('./APIResourcesConfiguration.js'),
+  resources: {
+    /**
+     * This resource provides an access token for your application consumes the Netdeal Resources
+     *
+     * @see http://www.netdeal.com.br/documentation/#authentication
+     */
+    requestAccessToken: '/open/request-token',
+
+    /**
+     * Integrate your consumers and leads with Netdeal through this resource
+     *
+     * @see http://www.netdeal.com.br/documentation/#data-integration
+     */
+    sendEntity: '/api/event/batch'
+  }
 };
