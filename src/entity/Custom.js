a0d64843c46843d17ed05f0d53ea5f0b8171f525
Maykonn <maykonn@outlook.com>
2018-10-09 18:11:19 -0300

    A customizable entity

---
+++ src/entity/Custom.js
@@ -0,0 +1,14 @@
+const Entity = require('./Entity.js');
+
+class Custom extends Entity {
+
+  /**
+   * This entity could have any string as key, for example PRODUCT, VIEW, etc.
+   */
+  constructor() {
+    super();
+  }
+
+}
+
+module.exports = Custom;
