a4bb767eba08f7aff81ccb08fb436f154d44e674
Maykonn <maykonn@outlook.com>
2018-10-09 18:55:32 -0300

    The key value is immutable

--- src/entity/Lead.js
+++ src/entity/Lead.js
@@ -1,7 +1,15 @@
 const Entity = require('./Entity.js');
 
 class Lead extends Entity {
+  constructor() {
+    super();
 
+    this._key = 'lead';
+  }
+
+  set key(value) {
+    throw new Error('The Lead entity key is immutable');
+  }
 }
 
 module.exports = Lead;
