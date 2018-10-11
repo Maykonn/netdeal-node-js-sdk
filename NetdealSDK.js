32b1f4d3316e86139c17e488887bf1efc9c4a390
Maykonn <maykonn@outlook.com>
2018-10-11 13:53:15 -0300

    Improved the exposed modules

--- NetdealSDK.js
+++ NetdealSDK.js
@@ -1,17 +1,30 @@
+const Configuration = require('./src/configuration/Configuration.js');
 const AccessToken = require('./src/AccessToken.js');
+
+/**
+ * The SDK exposed modules
+ *
+ * @type {{Configuration: Configuration, Lead: Lead, Consumer: Consumer, EntitiesCollection}}
+ */
+const Modules = {
+  Configuration: new Configuration(),
+  Lead: require('./src/entity/Lead.js'),
+  Consumer: require('./src/entity/Consumer.js'),
+  EntitiesCollection: require('./src/entity/EntitiesCollection.js')
+};
 
 /**
  * Netdeal Node.js SDK
  *
  * @author Maykonn Welington Candido <maykonn@outlook.com> (http://github.com/maykonn)
- * @type {{Configuration: Configuration, Consumer, ConsumersCollection, sendConsumer: function(Consumer), sendConsumers: function(ConsumersCollection)}}
+ * @type {{Configuration: Configuration, Lead: Lead, Consumer: Consumer, EntitiesCollection, sendEntities: function(EntitiesCollection)}}
  */
 module.exports = {
 
-  Configuration: require('./src/configuration/Configuration.js'),
-  Lead: require('./src/entity/Lead.js'),
-  Consumer: require('./src/entity/Consumer.js'),
-  EntitiesCollection: require('./src/entity/EntitiesCollection.js'),
+  /**
+   * The SDK exposed modules
+   */
+  ...Modules,
 
   /**
    * Send a collection of Consumers or Leads entities to Netdeal
@@ -19,7 +32,7 @@ module.exports = {
    * @param {EntitiesCollection} collection
    */
   sendEntities: (collection) => {
-    const accessToken = (new AccessToken(this.Configuration)).token;
+    const accessToken = (new AccessToken(Modules.Configuration)).token;
   }
 
 };
