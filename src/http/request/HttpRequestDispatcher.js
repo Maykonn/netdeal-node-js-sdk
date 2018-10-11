df1b5b9084d48432864385181fb68d2297813ccc
Maykonn <maykonn@outlook.com>
2018-10-11 16:38:24 -0300

    HTTP Request Dispatcher

---
+++ src/http/request/HttpRequestDispatcher.js
@@ -0,0 +1,64 @@
+const http = require('http');
+const HttpRequest = require('./HttpRequest.js');
+
+class HttpRequestDispatcher {
+  /**
+   * Execute a http request and awaits the response
+   *
+   * @param {HttpRequest} Request
+   * @return {Promise<*>}
+   */
+  static dispatch(Request) {
+    // TODO: create an HttpResponse object
+    return (async () => await this._executeHttpRequest({
+      host: Request.service.endpoint,
+      headers: Request.headers,
+      data: Request.data,
+      ...Request.resource // method and path
+    }).catch((error) => {
+      // TODO: create an HttpError object
+      //return error;
+    }))();
+  }
+
+  /**
+   * Execute the http request asynchronously
+   *
+   * @param options
+   * @return {Promise<*>}
+   * @private
+   */
+  static _executeHttpRequest(options) {
+    return new Promise((resolve, reject) => {
+      /**
+       * @param {http.ClientRequest}
+       */
+      const req = http.request(options);
+
+      const data = (
+        options.headers['Content-Type'] === 'application/json' ?
+          JSON.stringify(options.data) :
+          options.data
+      );
+
+      req.write(data);
+      req.end();
+
+      req.on('response', response => {
+        let body = '';
+        response.setEncoding('utf8');
+        response.on('data', chunk => body += chunk);
+        response.on('end', () => {
+          resolve((body ? JSON.parse(body) : body));
+        });
+      });
+
+      req.on('error', error => {
+        reject(error.message);
+      });
+    });
+  }
+
+}
+
+module.exports = HttpRequestDispatcher;
