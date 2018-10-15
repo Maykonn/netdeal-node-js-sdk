6381acd5bd283fd26f10cdb2e0f394de606941b6
Maykonn <maykonn@outlook.com>
2018-10-15 16:06:56 -0300

    Abstract object of a cache method

---
+++ src/infrastructure/cache/AbstractCache.js
@@ -0,0 +1,30 @@
+class AbstractCache {
+
+  /**
+   * AbstractCache
+   *
+   * @param {{host: string, port: number}} Configuration
+   */
+  constructor(Configuration) {
+    /**
+     * Cache configuration
+     *
+     * @type {{host: string, port: number}}
+     * @protected
+     */
+    this._configuration = Configuration;
+
+    /**
+     * An instance of a client implementation for a cache server for example:
+     * https://github.com/NodeRedis/node_redis or
+     * https://github.com/3rd-Eden/memcached
+     *
+     * @type {*}
+     * @private
+     */
+    this._client = undefined;
+  }
+
+}
+
+module.exports = AbstractCache;
