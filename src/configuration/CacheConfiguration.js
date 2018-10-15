af2d1c72abb7eaa4fe0ac75e79f557519fd2a362
Maykonn <maykonn@outlook.com>
2018-10-15 13:59:11 -0300

    Cache configuration file

---
+++ src/configuration/CacheConfiguration.js
@@ -0,0 +1,36 @@
+/**
+ * Cache configuration
+ *
+ * @type {{method: string, accessTokenKey: string, accessTokenKeyTTL: number, supportedMethods: {REDIS: string}}}
+ */
+module.exports = {
+
+  /**
+   * Default caching method
+   */
+  method: this.supportedMethods.REDIS,
+
+  /**
+   * The access token cache key
+   *
+   * @see http://www.netdeal.com.br/documentation/#authentication
+   */
+  accessTokenKey: 'Netdeal.AccessToken',
+
+  /**
+   * The access token will be stored with a TTL of 29 minutes to avoid API requests with 401 Unauthorized responses
+   *
+   * @see http://www.netdeal.com.br/documentation/#authentication
+   */
+  accessTokenKeyTTL: 1740, // 29 minutes
+
+  /**
+   * List of already implemented methods of caching.
+   * You can implement new methods of caching as memcached, mysql, mongodb, filesystem, etc,
+   * @see ../infrastructure/cache structure to do this.
+   */
+  supportedMethods: {
+    REDIS: 'redis'
+  }
+
+};
