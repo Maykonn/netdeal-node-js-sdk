169329a4a0b2312c406cd272b22d21e794230dc5
Maykonn <maykonn@outlook.com>
2018-10-25 16:46:50 -0300

    Refactored to use the global-process-queue-js package

--- NetdealSDK.js
+++ NetdealSDK.js
@@ -1,3 +1,4 @@
+const Process = require('global-process-queue-js');
 const Configuration = require('./src/configuration/Configuration.js');
 const AccessToken = require('./src/AccessToken.js');
 const CachingMethodFactory = require('./src/infrastructure/cache/CachingMethodFactory.js');
@@ -5,7 +6,7 @@ const CachingMethodFactory = require('./src/infrastructure/cache/CachingMethodFa
 /**
  * The SDK exposed modules
  *
- * @type {{Configuration: Configuration, Lead: Lead, Consumer: Consumer, EntitiesCollection}}
+ * @type {{Configuration: Configuration, Lead: Lead, Consumer: Consumer, EntitiesCollection: EntitiesCollection}}
  */
 const Modules = {
   Configuration: new Configuration(),
@@ -15,11 +16,40 @@ const Modules = {
 };
 
 /**
- * Cache method abstraction
- *
- * @type {AbstractRedisCache|AbstractCache}
+ * SDK Process Initializer
  */
-global.CacheClient = CachingMethodFactory.createCachingMethodInstance(Modules.Configuration.cache);
+const initializer = () => {
+  /**
+   * Caching method abstraction
+   *
+   * @type {AbstractRedisCache|AbstractCache}
+   */
+  global.CacheClient = CachingMethodFactory.createCachingMethodInstance(Modules.Configuration.cache);
+
+  /**
+   * Access Token provided by Netdeal, is required by all API requests
+   *
+   * @type {string}
+   */
+  global.AccessToken = '';
+};
+
+/**
+ * SDK Process Finisher
+ */
+const finisher = () => {
+  global.CacheClient.closeConnection();
+};
+
+/**
+ * SDK Processes Handler
+ *
+ * @type {GlobalProcess}
+ */
+const SystemFlow = new Process.Handler(initializer, finisher);
+
+
+let AccessTokenValue = '';
 
 /**
  * Netdeal Node.js SDK
@@ -40,10 +70,14 @@ module.exports = {
    * @param {EntitiesCollection} collection
    */
   sendEntities: async (collection) => {
-    const accessToken = await (new AccessToken(Modules.Configuration)).getToken();
-    console.log('accessToken', accessToken);
+    const test1 = (async () => {
+      AccessTokenValue = await(new AccessToken(Modules.Configuration)).getToken();
+      console.log('1accessToken', AccessTokenValue);
+      return '';
+    });
 
-    CacheClient.closeConnection();
+    SystemFlow.add(test1, Process.AWAIT);
+
+    SystemFlow.exec();
   }
-
 };
