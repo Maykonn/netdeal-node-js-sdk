c4365f3b9134efcb69e1fa4a9b50bc9a5d202ff5
Maykonn <maykonn@outlook.com>
2018-10-29 17:55:48 -0300

    Fixed entity

--- src/entity/Consumer.js
+++ src/entity/Consumer.js
@@ -8,6 +8,20 @@ class Consumer extends Entity {
     this._key = 'consumer';
   }
 
+  /**
+   * Retrieves the Entity key
+   *
+   * @return {string}
+   */
+  get key() {
+    return this._key;
+  }
+
+  /**
+   * "Abstract" method, could not be implemented
+   *
+   * @param {string} value
+   */
   set key(value) {
     throw new Error('The Consumer entity key is immutable');
   }
