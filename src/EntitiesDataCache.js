855c6f7817db67ef7ca938b5d9ae66c1e1f80802
Maykonn <maykonn@outlook.com>
2018-11-13 11:35:50 -0200

    Refactored

---
+++ src/EntitiesDataCache.js
@@ -0,0 +1,16 @@
+class DataCache {
+
+  constructor() {
+
+  }
+
+  /**
+   *
+   */
+  save(Entity) {
+    // iterate over the entity properties and with redis hset fill each necessary field on hash
+  }
+
+}
+
+module.exports = DataCache;
