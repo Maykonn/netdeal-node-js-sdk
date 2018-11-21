7ca5a45ae7adda587943fbca5fe0c28b817f1547
Maykonn <maykonn@outlook.com>
2018-11-21 16:20:15 -0200

    HttpResponse data

--- src/http/response/HttpResponse.js
+++ src/http/response/HttpResponse.js
@@ -1,4 +1,21 @@
 class HttpResponse {
+
+  /**
+   * HttpResponse
+   * @param body
+   */
+  constructor(body) {
+    this.body = body;
+  }
+
+  get body() {
+    return this._body;
+  }
+
+  set body(value) {
+    this._body = value;
+  }
+
 }
 
 module.exports = HttpResponse;
